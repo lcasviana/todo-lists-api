d233b9186be548e5ef10690e4fdfa3615494c454
Lucas Viana <lcasviana@gmail.com>
2020-07-26 21:11:51 -0300

    uppercase

--- src/database/mongo.database.js
+++ src/database/mongo.database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(_ => console.log('mongodb connected'))
   .catch(err => console.error(err.message));
 
