b21c8452c27c88b5679fbaed2a3dd4fff41582d5
Lucas Viana <lcasviana@gmail.com>
2020-07-26 21:09:47 -0300

    port 9000

--- src/index.js
+++ src/index.js
@@ -17,5 +17,5 @@ const userRoute = require('./routes/user.route');
 app.use('/api/accounts', accountRoute);
 app.use('/api/users', userRoute);
 
-const port = process.env.port || 80;
+const port = process.env.port || 9000;
 app.listen(port, () => console.log(`api port ${port}`));
